08c17ca4713b246cc2f86f6c0f406f308bd2b523
Ziadali20 <ziadali4416@gmail.com>
2023-07-05 06:11:24 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,105 @@
+// Assignment-1 JS
+
+/* start Q1:
+
+
+            var x= Number(window.prompt("Enter a number : "));
+            console.log(x);
+
+End Q1*/
+
+
+
+/*start Q2:
+
+            var x= Number(window.prompt("Enter a number : "));
+            if(x%3==0&&x%4==0)
+            {
+                console.log("Yes");
+            }
+            else
+            {
+                console.log("No");
+            }
+
+End Q2*/
+
+
+/*start Q3:
+
+            var x= Number(window.prompt("Enter number 1: "));
+            var y= Number(window.prompt("Enter number 2: "));
+            if(x>y)
+            {
+                console.log(x);
+            }
+            else if(x==y)
+            {
+                console.log("Both are equal");
+            }
+            else{
+                console.log(y);
+            }
+End Q3*/
+
+
+/*start Q4:
+
+            var x= Number(window.prompt("Enter a number"));
+            if(x>0)
+            {
+                console.log("Positve");
+            }
+            else{
+                console.log("Negative");
+            }
+End Q4*/
+
+/*start Q6:
+
+            var x= Number(window.prompt("Enter a number"));
+            if(x%2==0)
+            {
+                console.log("Even");
+            }
+            else{
+                console.log("Odd");
+            }
+End Q6*/
+
+
+ /*start Q8:
+
+            var x= window.prompt("Drop a letter");
+            if(x=='a'||x=='A'||x=='o'||x=='O'||x=='u'||x=='U'||x=='e'||x=='E'||x=='I'||x=='i')
+            {
+                console.log("Vowel");
+            }
+            else{
+                console.log("Constant");
+            }
+ End Q8*/
+
+
+/* start Q9:
+
+        var x= Number(window.prompt("Enter a number"));
+        for(i=1;i<=x;i++)
+        {
+            console.log(i);
+        }
+
+
+ End Q9*/
+
+
+/* start Q10:
+
+        var x= Number(window.prompt("Enter a number"));
+        for(i=1;i<=12;i++)
+        {
+            console.log(x*i);
+        }
+
+
+ End Q10*/
